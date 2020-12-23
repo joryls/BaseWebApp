4ea6c8d416d80f0faef06630fed386babebd8932
Jory Soderman <joryls@gmail.com>
2020-12-22 21:06:53 -0600

    clean up

---
+++ project/js/main.js
@@ -0,0 +1,9 @@
+//reading data from cc database 
+
+//create firebase instance
+var database = firebase.database();
+
+var userId = firebase.auth().currentUser.uid;
+  console.log(firebase.database().ref('/users/' + userId).once('value').then((snapshot) => {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+  }))
