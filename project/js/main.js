477883cd5fe78ef601e57ab661ef5ffdea722891
Jory Soderman <joryls@gmail.com>
2020-12-23 19:38:05 -0600

    it's alive

--- project/js/main.js
+++ project/js/main.js
@@ -7,3 +7,14 @@ var userId = firebase.auth().currentUser.uid;
   console.log(firebase.database().ref('/users/' + userId).once('value').then((snapshot) => {
     var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
   }))
+
+ref.once("value", function(data) {
+    // do some stuff once
+  });
+
+//sign out user
+  /* firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });   */
--- project/start.js
+++ project/start.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/merchapp.ejs', function(request, response) {
+  response.render('pages/merchapp');
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
