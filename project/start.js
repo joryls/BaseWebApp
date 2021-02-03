8b669a85ce311a76983614c4517a246c9b7e8eb4
Jory Soderman <joryls@gmail.com>
2021-02-03 09:41:23 -0600

    test app beta

--- project/start.js
+++ project/start.js
@@ -1,12 +1,3 @@
-//const functions = require('firebase-functions');
-//const cors = require('cors');
-
-/* exports.addMessage = functions.https.onRequest((req, res) => {
-  cors()(req, res, () => {
-    return res.json({status: 'ok'});
-  });
-}); */
-
 var express = require('express');
 var app = express();
 
@@ -27,10 +18,14 @@ app.get('/merchapp.ejs', function(request, response) {
 });
 
 
-app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+/* process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('App exited');
+  })
+}) */
 // This file is what handles incoming requests and
 // serves files to the browser, or executes server-side code
